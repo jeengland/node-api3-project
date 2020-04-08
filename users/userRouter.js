ccfc609ac11227f272475868b03982386089a843
unknown <jacob.e.england@gmail.com>
2020-04-08 15:03:36 -0500

    Added validateUser and validatePost middleware

--- users/userRouter.js
+++ users/userRouter.js
@@ -10,12 +10,12 @@ const router = express.Router();
 // endpoints
 // ----- BASE URL: /api/users -----
 
-router.post('/', (req, res) => {
-
+router.post('/', validateUser, (req, res) => {
+  res.status(200).send('validateUser is working!')
 });
 
-router.post('/:id/posts', (req, res) => {
-  // do your magic!
+router.post('/:id/posts', [validateUserId, validatePost], (req, res) => {
+  res.status(200).send('validatePost is working!')
 });
 
 router.get('/', (req, res) => {
@@ -43,7 +43,7 @@ router.put('/:id', (req, res) => {
 function validateUserId(req, res, next) {
   const id = req.params.id;
   db.getById(id)
-    .then((valid)=> {
+    .then((valid) => {
       if (valid) {
         next();
       } else  {
@@ -52,14 +52,46 @@ function validateUserId(req, res, next) {
         })
       }
     })
+    .catch((error) => {
+      console.error(error);
+      res.status(500).json({
+        errorMessage: 'User was not able to be validated'
+      })
+    })
 }
 
 function validateUser(req, res, next) {
-  // do your magic!
+  const body = req.body;
+  if (Object.keys(body).length) {
+    if (body.name) {
+      next();
+    } else {
+      res.status(400).json({
+        errorMessage: 'Missing required name field!'
+      })
+    }
+  } else {
+    res.status(400).json({ 
+      errorMessage: 'Missing user data!'
+    });
+  }
 }
 
 function validatePost(req, res, next) {
-  // do your magic!
+  const body = req.body;
+  if (Objects.keys(body).length) {
+    if (body.text) {
+      next();
+    } else {
+      res.status(400).json({
+        errorMessage: 'Missing required text field!'
+      })
+    }
+  } else {
+    res.status(400).json({
+      errorMessage: 'Missing post data!'
+    })
+  }
 }
 
 module.exports = router;
